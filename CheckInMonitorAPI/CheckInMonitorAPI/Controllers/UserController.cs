f6086bb02c5b127ffd7d6641ab50dee51946200e
snitzel_klaus <SnitzelKlaus@gmail.com>
2023-09-19 15:14:05 +0200

    Update UserController.cs

--- CheckInMonitorAPI/CheckInMonitorAPI/Controllers/UserController.cs
+++ CheckInMonitorAPI/CheckInMonitorAPI/Controllers/UserController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CheckInMonitorAPI.Models.Entities;
+using CheckInMonitorAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CheckInMonitorAPI.Controllers
 {
+    [Route("api/[controller]")]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IUserService _userService;
+        private readonly IRoleService _roleService;
+
+        public UserController(IUserService userService, IRoleService roleService)
         {
-            return View();
+            _roleService = roleService;
+            _userService = userService;
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] CreateUserDto createUserDto)
+        {
+            if (createUserDto == null)
+            {
+                return BadRequest("User data cannot be null");
+            }
+
+            // Assuming you have a method to convert DTO to User entity
+            var user = MapDtoToUser(createUserDto);
+
+            await _userService.AddAsync(user);
+
+            return CreatedAtAction(nameof(GetUserById), new { id = user.Id }, user);
+        }
+
     }
 }
