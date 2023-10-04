ea72e66589ac56995f19006a9eae6bed8882ec1e
Tobias Lilsby <92530784+TPLilsby@users.noreply.github.com>
2023-10-04 10:03:59 +0200

    api

--- CheckInMonitorAPI/CheckInMonitorAPI/Models/Entities/User.cs
+++ CheckInMonitorAPI/CheckInMonitorAPI/Models/Entities/User.cs
@@ -38,7 +38,7 @@ namespace CheckInMonitorAPI.Models.Entities
         public bool Preoccupied { get; set; }
 
         [Required]
-        public TimeOnly MeetingTime { get; set; }
+        public DateTime? MeetingTime { get; set; }
 
         [Required]
         public int RoleId { get; set; }
